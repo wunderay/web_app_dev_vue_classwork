d863e89ae314622342dc02c5cd96166325c507f3
Wunderay <60483832+wunderay@users.noreply.github.com>
2021-03-17 23:04:19 -0600

    Assignment 2 file submission

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -10,8 +10,16 @@ app.component('book-display', {
         /*html*/
         `<ul class="col-sm-12 col-md-6 col-xl-4">
          <a class="list-group-item list-group-item-action active" :href="this.bookObj.selfLink"> {{this.bookObj.volumeInfo.title}} </a>
-         <li class="list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
-         <li class="list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
+         <img class="list-group-item" :src='this.bookObj.volumeInfo.imageLinks.thumbnail'>
+         <li class="list-group-item" v-if="this.bookObj.volumeInfo.authors != null && this.bookObj.volumeInfo.authors != undefined">
+            Author(s): <span v-for="author in this.bookObj.volumeInfo.authors">{{author}}{{(author != this.bookObj.volumeInfo.authors[this.bookObj.volumeInfo.authors.length-1])?', ':''}}</span>
+         </li>
+         <li class="list-group-item">{{(this.bookObj.volumeInfo.publisher != null && this.bookObj.volumeInfo.publisher != undefined)?'Publisher: ' + this.bookObj.volumeInfo.publisher + '; ':''}}
+         {{this.bookObj.volumeInfo.publishedDate}}</li>
+         <li class="list-group-item" v-if="this.bookObj.volumeInfo.categories != null && this.bookObj.volumeInfo.categories != undefined">
+            Genre(s): <span v-for="category in this.bookObj.volumeInfo.categories">{{category}}</span> 
+        </li>
+         <li class="list-group-item" v-if="this.bookObj.volumeInfo.pageCount != null && this.bookObj.volumeInfo.pageCount != undefined">{{this.bookObj.volumeInfo.pageCount}} Pages</li>
          </ul>`,
     computed: {
         bookObj(){
--- js/main.js
+++ js/main.js
@@ -2,14 +2,28 @@ const app = Vue.createApp({
     data() {
         return {
             keyword: '',
-            result: null
+            result: null,
+            pageNum: 0,
+            maxPages: -1
         }
     },
     methods: {
         searchGoogleBooks(){
-            fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.keyword + "&startIndex=0&maxResults=20")
+            fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.keyword + "&startIndex=" + this.pageNum * 20 + "&maxResults=20")
                 .then(response => response.json())
                 .then(json => this.result = json)
+                .then(json => {maxPages = Math.floor(json.totalItems/20);
+                                console.log(json.totalItems);
+                                console.log(this.pageNum);
+                                console.log(this.maxPages);
+                })
+                .then(scrollTop())
         }
     }
 })
+function scrollTop () {
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
+    window.scrollTo(0,0)
+}
